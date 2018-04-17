cade0b8b472e9e4092bdff07ed85c0e7a7543114
Hamidah <hamidaha@l5lab.com>
2018-04-17 15:04:20 +0100

    initial commit

---
+++ login.html
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+	<div class="fpassword">
+		<label>Email</label><br>
+			<input type="email" name="email"><br>
+		<label>Password</label><br>
+			<input type="password" name="password">
+		<button><a href="#">Login</a></button>
+	</div>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sample</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+	<div class="box">
+		<form>
+			<a href="#">SignIm</a>
+			<a href="#top">login</a>
+			<label>Name</label>
+				<input type="text" name="name" ><br>
+			<label>Email</label>
+				<input type="email" name="email"><br>
+			<label>Password</label>
+				<input type="password" name="password"><br>
+			<label>Confirm Password</label>
+				<input type="password" name="Confirm_password"><br>
+
+			<button>Submit</button><br>
+			
+			
+		</form>
+		
+		s
+	</div>
+</body>
+</html>
